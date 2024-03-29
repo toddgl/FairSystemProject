94cf3c53af9e75ed6efbbb925a4c2074362a4652
Glenn Todd <g.todd@internet.co.nz>
2024-03-29 20:33:53 +1300

    Adjusted the allowed URLs to .env file

--- config/settings.py
+++ config/settings.py
@@ -34,9 +34,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['192.168.1.83', 'ruru', 'localhost', '127.0.0.1', ]
-
-
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 # Application definition
 
