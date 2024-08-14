55e02c40c19760f8e80a60604697dd12aaae5785
Glenn <g.todd@internet.co.nz>
2024-08-15 08:27:11 +1200

    Modified site_allocations function to account for more recent site allocations, where a stallholder has switched sites and should be allocated the most recent site if they have been there for at least two years

--- utils/site_allocation_tools.py
+++ utils/site_allocation_tools.py
@@ -21,7 +21,7 @@ db_logger = logging.getLogger('db')
 
 def site_allocation_emails():
     """
-    Function to create stallholder emails to advise them that they have been preallocted a site based on their
+    Function to create stallholder emails to advise them that they have been pre-allocated a site based on their
     registration history asking them to apply for the fair before the activation date. The function is called from
     the management process dashboard . Processing information is recorded in the CustomDBLogger table which can be
     viewed using Django Admin
@@ -50,79 +50,75 @@ def site_allocation_emails():
 
 
 def site_allocations():
-    """
-    Function to create stallholder site allocations based on their historical registration site usages.
-    The function is called from the management process dashboard and creates new instances in the SiteAllocation
-    database table. Processing information is recorded in the CustomDBLogger table which can be viewed using Django Admin.
-    """
     df = pd.DataFrame.from_records(SiteHistory.fouryearhistorymgr.all().values())
     df['year'] = pd.to_datetime(df['year'], format='%Y')
     today = pd.to_datetime('today')
     begin = today - pd.offsets.Day(4 * 365)
     four_year_data = df[df['year'] > begin]
-    """
-    Create subset list where the stallholder has had the same site 4 years, 3 years and 2 years in row and finally 
-    where the stallholder has only had a site a single time
-    Process the stallholder site series for each current future event to create new SiteAllocation instances checking 
-    to make sure that the EventSite site_status is set to Available this should be sufficient to prevent duplicates from 
-    being created without resorting to test for duplicates before a save 
-    """
 
     events = Event.currenteventfiltermgr.all()
     count = 4
+
     while count > 0:
-        # Group by stallholder_id and site_id, then count the occurrences
         group_counts = four_year_data.groupby(['stallholder_id', 'site_id']).size()
-        # Filter those that have exactly 4 entries (indicating 4 different years)
-        year_sites = group_counts[group_counts == count]
+        year_sites = group_counts[group_counts >= 2]  # Prioritize sites with at least 2 occurrences
 
-        for stallholder_id, site_id in year_sites.index:
-            # Fetch the latest SiteHistory record for this stallholder and site
-            site_histories = SiteHistory.objects.filter(stallholder_id=stallholder_id, site_id=site_id)
-            # Use the most recent history
-            recent_history = site_histories.order_by('-year').first()
-            is_half_size = recent_history.is_half_size
+        for stallholder_id in year_sites.index.get_level_values('stallholder_id').unique():
+            # Get site allocation history for the stallholder sorted by year
+            stallholder_history = four_year_data[four_year_data['stallholder_id'] == stallholder_id]
+            recent_sites = stallholder_history.groupby('site_id').agg({
+                'year': 'max',
+                'stallholder_id': 'count'
+            }).sort_values(by=['year', 'stallholder_id'], ascending=[False, False])
 
-            # Determine the required site size based on the is_half_size flag
-            required_site_size = 'Half Size Fair Site' if is_half_size else 'Full Size Fair Site'
-            site_name = Site.objects.get(id=site_id).site_name
+            # First try the most recent site with the highest count in the last 2 years
+            preferred_site_id = recent_sites.index[0]
+            fallback_site_id = recent_sites.index[1] if len(recent_sites) > 1 else None
 
-            for event in events:
-                if EventSite.objects.filter(event_id=event.id, site_id=site_id,
-                                            site__site_size__item_name=required_site_size).exists():
-                    eventsite = EventSite.objects.get(event_id=event.id, site_id=site_id)
-                    # Check to see if stallholder has an allocation for this event (note not the eventsite) in
-                    #  that case don't create another allocation
-                    if SiteAllocation.objects.filter(event_site__event__id=eventsite.event.id,
-                                                  stallholder__id=stallholder_id).exists():
-                        db_logger.info('Siteallocation already exists for Stallholder {} in Event {} '.format( stallholder_id, eventsite.event.event_name))
-                    else:
-                        if eventsite.site_status == 1:
-                            SiteAllocation.objects.create(
-                                stallholder_id=stallholder_id,
-                                event_site_id=eventsite.id,
-                                created_by_id=3,
-                            )
-                            eventsite.site_status = 2
-                            eventsite.save()
+            for site_id in [preferred_site_id, fallback_site_id]:
+                if site_id is None:
+                    continue
+                site_histories = SiteHistory.objects.filter(stallholder_id=stallholder_id, site_id=site_id)
+                recent_history = site_histories.order_by('-year').first()
+                is_half_size = recent_history.is_half_size
+                required_site_size = 'Half Size Fair Site' if is_half_size else 'Full Size Fair Site'
+                site_name = Site.objects.get(id=site_id).site_name
+
+                for event in events:
+                    if EventSite.objects.filter(event_id=event.id, site_id=site_id,
+                                                site__site_size__item_name=required_site_size).exists():
+                        eventsite = EventSite.objects.get(event_id=event.id, site_id=site_id)
+                        if SiteAllocation.objects.filter(event_site__event__id=eventsite.event.id,
+                                                         stallholder__id=stallholder_id).exists():
+                            db_logger.info(f'Siteallocation already exists for Stallholder {stallholder_id} in Event {eventsite.event.event_name}')
                         else:
-                            db_logger.warning(
-                                f'SiteAllocation for Stallholder ID {stallholder_id} Event Name {event.event_name} and '
-                                f'Site name {eventsite.site.site_name} has not been created, as the EventSite has been taken.',
-                                extra={'custom_category': 'Site Allocation'})
-                else:
-                    db_logger.warning(
-                        f'SiteAllocation for Stallholder ID {stallholder_id} Event Name {event.event_name} and Site '
-                        f'name {site_name} and Site Size {required_site_size} has not been created, as the EventSite does not exist.',
-                        extra={'custom_category': 'Site Allocation'})
+                            if eventsite.site_status == 1:
+                                SiteAllocation.objects.create(
+                                    stallholder_id=stallholder_id,
+                                    event_site_id=eventsite.id,
+                                    created_by_id=3,
+                                )
+                                eventsite.site_status = 2
+                                eventsite.save()
+                            else:
+                                db_logger.warning(
+                                    f'SiteAllocation for Stallholder ID {stallholder_id} Event Name {event.event_name} and '
+                                    f'Site name {eventsite.site.site_name} has not been created, as the EventSite has been taken.',
+                                    extra={'custom_category': 'Site Allocation'})
+                    else:
+                        db_logger.warning(
+                            f'SiteAllocation for Stallholder ID {stallholder_id} Event Name {event.event_name} and Site '
+                            f'name {site_name} and Site Size {required_site_size} has not been created, as the EventSite does not exist.',
+                            extra={'custom_category': 'Site Allocation'})
         count -= 1
+
 
 def delete_unregistered_allocations():
     """
-    Function to delete stallholder site allocations that have not been associated with a registration record.
-    The function is called from the management process dashboard and removes records from the SiteAllocation
-    database table. It will not remove record that are assocciated with a registration, or if the record has its on_hold flag set.
-    processing information is recorded in the CustomDBLogger table which can be viewed using Django Admin
+    Function to delete stallholder site allocations that have not been associated with a registration record. The
+    function is called from the management process dashboard and removes records from the SiteAllocation database
+    table. It will not remove record that are associated with a registration, or if the record has its on_hold flag
+    set. processing information is recorded in the CustomDBLogger table which can be viewed using Django Admin
     """
     unregistered_allocations = SiteAllocation.objects.filter(stall_registration__isnull=True, on_hold=False)
     if unregistered_allocations:
@@ -133,4 +129,5 @@ def delete_unregistered_allocations():
                 allocation.delete()
                 eventsite.save()
             except Exception as e:  # It will catch other errors related to the delete call.
-                db_logger.error('There was an error deleting the unregistered site allocations.' + e, extra={'custom_category': 'Site Allocations'})
+                db_logger.error('There was an error deleting the unregistered site allocations.' + e,
+                                extra={'custom_category': 'Site Allocations'})
